fc85687a47a3217eeabaf8a3050429b9ad73e4d0
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-17 14:33:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstWord(s) {
   // your code here
+	s=s.trim();
+if(s === '' ){
+return '';
+}	
+	let res='';
+	for (let i = 0; i < s.length; i++) {
+		if(s[i]==' '){
+          return res;
+		}
+		res+=s[i];
+	}
+	
 }
 
 // Do not change the code below
